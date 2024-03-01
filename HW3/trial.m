e8329708ed5a9ca6080b23a4dd770633c658f98e
Mauro Patimo <mauro.patimo@gmail.com>
2024-03-01 01:09:26 -0500

    HW3

---
+++ HW3/trial.m
@@ -0,0 +1,4 @@
+x = rand(1, 100); % Generate 100 random values between 0 and 1
+y = rand(1, 100); % Generate 100 random values between 0 and 1
+
+scatter(x, y); % Plot the random values as a scatter plot
